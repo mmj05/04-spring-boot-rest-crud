f0a76c0bbcd1cd45d4b1df74a930b450e9c4e650
Muhaimin Jobayer <mmj0993@gmail.com>
2023-08-17 20:25:11 -0500

    change application properties

---
+++ 04-spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/jobayer/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.jobayer.springboot.cruddemo.dao;
+
+import com.jobayer.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+// @RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
